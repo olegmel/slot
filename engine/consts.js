6de60c285ea09ebdab9964a0554c6d567da12823
Cameron Banowsky <cameron@hack.cd>
2015-06-21 10:44:37 -0700

    engine logic added

---
+++ engine/consts.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Cameron Banowsky on 6/21/2015.
+ */
+/**
+ * Created by Sergey on 17.06.2015.
+ */
+function consts(reelCount, rewCount, symbolCount, emptySymbol, pays, payback, lineCount) {
+    this.reelCount = reelCount;
+    this.rowCount = rewCount;
+    this.symbolCount = symbolCount;
+    this.emptySymbol = emptySymbol;
+    this.pays = pays;
+    this.payback = payback;
+    this.lineCount = lineCount;
+}
